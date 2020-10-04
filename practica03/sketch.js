8b28000f1b1f013c88b6598c59c758cf3bc8ebd7
sihuintaperez <38123330+sihuintaperez@users.noreply.github.com>
2020-10-03 23:05:03 -0500

    Create sketch.js

---
+++ practica03/sketch.js
@@ -0,0 +1,81 @@
+
+/****
+***** Ractangulo aleatorio
+						*****/
+
+let qt;
+let count = 0;
+
+
+function setup ()
+{
+	createCanvas (400 ,400) ;
+	let boundary = new Rectangle (200 ,200 ,200 ,200) ;
+	qt = new QuadTree ( boundary , 4) ;
+	console .log (qt);
+	
+	for (let i =0; i < 100; i ++) {
+		let p = new Point ( Math . random () * 400 , Math . random () * 400) ;
+		qt. insert (p);
+	}
+	background (0) ;
+	qt. show () ;
+
+	stroke (0 ,255 ,0) ;
+	rectMode ( CENTER );
+	
+	let range = new Rectangle ( random (200) ,random (200) ,random (50) ,random (50) )
+	//let range = new Rectangle(200,200,100,100);
+	rect ( range .x, range .y, range .w*2 , range .h *2) ;
+	let points = [];
+	
+	qt. query (range , points );
+	for (let p of points ){
+		strokeWeight (4) ;
+		point (p.x, p.y);
+	}
+	console .log ( count );
+}
+
+/****
+***** Ractangulo con maouse
+						*****/
+
+/*
+let qt;
+
+let count = 0;
+
+function setup () {
+	createCanvas (400 ,400) ;
+	let boundary = new Rectangle (200 ,200 ,200 ,200) ;
+	qt = new QuadTree ( boundary , 4) ;
+	console .log (qt);
+	for (let i =0; i < 25; i ++) {
+		let p = new Point ( Math . random () * 400 , Math . random () * 400) ;
+		qt. insert (p);
+	}
+	background (0) ;
+	qt. show () ;
+}
+function draw () {
+	background (0) ;
+	qt. show () ;
+	
+	stroke (0 ,255 ,0) ;
+	rectMode ( CENTER );
+	
+	let range = new Rectangle ( mouseX ,mouseY ,50 ,50)
+	rect ( range .x, range .y, range .w*2 , range .h *2) ;
+	let points = [];
+	
+	
+	qt. query (range , points );
+
+	for (let p of points ){
+		strokeWeight (4) ;
+		point (p.x, p.y);
+	}
+	 console . log ( count +" ");
+}
+*/
